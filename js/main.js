46f3cf9a822469fe802202cac5057de2223e4947
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-10-01 23:14:11 +0200

    js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.table(movies2);
 
 for (val of movies2){
     document.getElementById("row").innerHTML += `
-    <div class="card col-4  p-0" style="max-width: 540px">
+    <div class="card col-lg-4 col-md-6 p-0" >
           <div class="row g-0 cardRow">
             <div class="col-md-4">
               <img
@@ -12,18 +12,18 @@ for (val of movies2){
                 alt="John Wick"
               />
             </div>
-            <div class="col-md-8 bg-dark text-light ">
+            <div class="col-md-8 text-light position-relative backgroundColor">
               <div class="card-body pe-lg-1 pb-0 ">
                 <h4 class="card-title "> ${val.movieName}</h4>
                 <p class="card-text"><small> ${val.description}
                   </small>
                 </p>
-                <div class=" d-flex justify-content-end bottom pt-3 pe-2 pb-1   ">
-                    <button type="button" class="btn  bg-transparent text-success fs-4 pt-1">Like 
-                        <span class="btn-label"><i class="fa fa-thumbs-up"></i></span></button>
-                        <div class="circle"><span id="circleText"> ${val.likes}</span> </div>
-                  
-                </div>
+                <p class="mb-0">Release Date: ${val.releaseDate}</p>
+                <div class=" d-flex cardBottom position-absolute justify-content-end w-100 pe-2 pb-2 " >
+                <button type="button" class="btn  bg-transparent text-success fs-5  text-end ">Like 
+                    <span class="btn-label"><i class="fa fa-thumbs-up"></i></span></button>
+                    <div class="circle"><span id="circleText"> ${val.likes}</span></div>
+            </div>
               </div>
             </div>
           </div>
